7b55ea6345b97ad239f5a3fb64da736c0735a621
ChelsyA <63288081+ChelsyA@users.noreply.github.com>
2020-08-21 15:43:55 +0000

    Update ApprovalCard.js
    edited ApprovalCard

--- reactjsx/src/ApprovalCard.js
+++ reactjsx/src/ApprovalCard.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Convert to class based component
-class SearchBar extends React.Component {
-  state = { term: ''};
 
-  
-  render(){
+// const ApprovalCard = (props) => {
+//   return (
+//     <div className="ui card">
+//       <div className="content">{props.children}</div>
+//       <div className="extra content">
+//         <div className="ui two buttons">
+//           <div className="ui basic green button">Approve</div>
+//           <div className="ui basic red button">Reject</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ApprovalCard;
+
+class ApprovalCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
-    <div className="ui card">
-      <div className="content">{props.children}</div>
-      <div className="extra content">
-        <div className="ui two buttons">
-          <div className="ui basic green button">Approve</div>
-          <div className="ui basic red button">Reject</div>
+      <div className="ui card">
+        <div className="content">{this.props.children}</div>
+        <div className="extra content">
+          <div className="ui two buttons">
+            <div className="ui basic green button">Approve</div>
+            <div className="ui basic red button">Reject</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-    };
-};
+    );
+  }
+}
 
 export default ApprovalCard;
